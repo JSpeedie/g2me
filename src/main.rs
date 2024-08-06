cdc224312c395272c1646e98229dd79c0c020a6b
JSpeedie <julianspeedie@gmail.com>
2024-08-05 21:56:09 -0400

    Added the main files of the project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// Imports
+use glicko2::*;
+
+
+
+fn main() {
+    // let rating_period_ht: hashmap;
+    let rating_period_ht: i32 = 0;
+
+    read_file(&mut rating_period_ht);
+
+    println!("result from _g is: {test}");
+}
